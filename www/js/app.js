fe08f1533e9fc0e84359eab934ef57ad86882fd3
Tyler Fisher <tfisher@npr.org>
2016-11-14 17:52:57 -0500

    v simple modal

--- www/js/app.js
+++ www/js/app.js
@@ -1,10 +1,32 @@
 import Barba from 'barba.js';
 
+let songContainers = null;
+let modal = null;
+let modalOverlay = null;
+
 const onWindowLoaded = function() {
-    /**
-     * Next step, you have to tell Barba to use the new Transition
-     */
+    songContainers = document.querySelectorAll('.song-container');
+    modal = document.querySelector('.modal');
+    modalOverlay = document.querySelector('.modal-overlay');
+
+    for (var i = 0; i < songContainers.length; i++) {
+        songContainers[i].addEventListener('click', onSongClick);
+    }
+    modalOverlay.addEventListener('click', onModalClick);
+
     Barba.Pjax.start();
+}
+
+const onSongClick = function() {
+    modal.style.display = 'block';
+
+    modal.querySelector('.artist').innerHTML = this.querySelector('h6').innerHTML;
+    modal.querySelector('.title').innerHTML = this.querySelector('h5').innerHTML;
+    modal.querySelector('.desc').innerHTML = this.querySelector('p').innerHTML;
+}
+
+const onModalClick = function() {
+    modal.style.display = 'none';
 }
 
 window.onload = onWindowLoaded;
